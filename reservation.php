b162af64a6ffce80c1fdc8e889ca78993a9675d4
harryboterkoek <michaelvanjelgerhuis@gmail.com>
2015-06-25 15:23:31 +0200

    Swiggity swooty I'm coming for that booty

--- reservation.php
+++ reservation.php
@@ -67,6 +67,48 @@
         </div>
         <!-- Navigation bar end -->
     </nav>
+    <div class="container">
+        <div class="row">
+            <form class="col s12 m6 offset-m3" autocomplete="off" action="" method="post">
+                <div class="row grey lighten-3 post-index z-depth-2">
+                    <h5 class="flow-text" style="text-align:center;">
+                        Profile
+                        <div class="line-separator red darken-4"></div>
+                    </h5>
+
+                    <div class="input-field col s10 offset-s1">
+                        <input id="name" name="name" type="text" class="validate" value="<?php echo escape($user->data()->name); ?>">
+                        <label for="name">Name</label>
+                    </div>
+
+                    <div class="input-field col s10 offset-s1">
+                        <input id="email" name='email' type="email" class="validate" value="<?php echo escape($user->data()->email); ?>">
+                        <label for="email">Email</label>
+                    </div>
+
+                    <div class="input-field col s10 offset-s1">
+                        <input id="location" name="location" type="text" class="validate" value="<?php echo escape($user->data()->location); ?>">
+                        <label for="location">Location</label>
+                    </div>
+
+
+                    <div class="input-field col s10 offset-s1">
+                        <textarea id="summary" name="summary" class="materialize-textarea"><?php echo escape($user->data()->summary);?></textarea>
+                        <label for="summary">Summary</label>
+                    </div>
+
+                    <div class="col s10 offset-s1">
+                        <button class="btn waves-effect red waves-light lighten-1 col s6 offset-s3" type="submit">Edit profile<i class="mdi-action-input right"></i></button>
+                        <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+                    </div>
+                </div>
+            </form>
+        </div>
+
+
+
+<input type="date" class="datepicker">
+
     </div>
     <script type="text/javascript" src="resources/js/jquery-1.11.3.min.js"></script>
     <script type="text/javascript" src="resources/js/materialize.min.js"></script>
@@ -76,6 +118,10 @@
 
         $(".dropdown-button").dropdown();
 
+         $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+          });
     </script>
 </body>
 </html>
