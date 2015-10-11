d4f70aaf66f986c2d3b26307e89d9194371b8782
Michael van Jelgerhuis <michaelvanjelgerhuis@gmail.com>
2015-10-12 00:42:29 +0200

    Reserve class, moeten wat dingen oplossen voordat we verder kunnen met programmeren.

---
+++ classes/Reserve.php
@@ -0,0 +1,13 @@
+<?php
+class Reserve {
+    private $_db,
+            $_data;
+
+    public function __construct($user = null){
+        $this->_db = DB::getInstance();
+    }
+
+    public function fetch($code = null) {
+        }
+    }
+
