a2d2c07a5f33e62db5dd8e83a36abac96bf06e98
Michael van Jelgerhuis <michaelvanjelgerhuis@gmail.com>
2015-10-16 01:55:39 +0200

    Progress on Reservation page

--- reservation.php
+++ reservation.php
@@ -1,19 +1,34 @@
 <?php
-/*
-    Workplace DB vullen met de tafelgroepen
+    /*
+        Workplace DB vullen met de tafelgroepen
 
-    Inserten reservering
-        - Eerst controleren of er op de aangegeven datum/tijd een werkplek beschikbaar is. d.m.v. een query
-        - Tijd id 1/2 aangegeven in reserveringsform 1 query uitvoeren, als tijd id 3 is aangegeven in reserveringsform 2 queries uitvoeren voor tijdid 1/2
-        - Reserveringen in database inserten d.m.v. query
- */
-require('core/init.php');
+        Inserten reservering
+            - Eerst controleren of er op de aangegeven datum/tijd een werkplek beschikbaar is. d.m.v. een query
+            - Tijd id 1/2 aangegeven in reserveringsform 1 query uitvoeren, als tijd id 3 is aangegeven in reserveringsform 2 queries uitvoeren voor tijdid 1/2
+            - Reserveringen in database inserten d.m.v. query
+     */
+    require('core/init.php');
 
-$user = new User();
+    $user = new User();
 
-if(!$user->isLoggedIn()){
-    Redirect::to( 'login.php');
-}
+    if(!$user->isLoggedIn()){
+        Redirect::to( 'login.php');
+    }
+
+    if(Input::exists()) {
+        if(Token::check(Input::get('token'))){
+            $validate = new Validate();
+            $valdidate = $validate->check($_POST, array(
+                'date' => array(
+                    'required' => true
+                ),
+                'time' => array(
+                    'required' => true
+                )
+            ));
+        }
+
+    }
 ?>
 
 <!DOCTYPE html>
@@ -88,25 +103,26 @@ if(!$user->isLoggedIn()){
                     <h6 class="flow-text center">
                         Reserveren
                         <div class="line-separator red darken-4"></div>           </h6>
-                    <input type="date" placeholder="Klik hier om een datum te selecteren" class="datepicker">
+                    <input name="date" type="date" placeholder="Klik hier om een datum te selecteren" class="datepicker">
                     <p>Selecteer het gewenste tijdsvenster:</p>
                     <div class="row">
 
                         <p>
-                            <input name="group1" class="blue" type="radio" id="1" />
+                            <input name="time" class="blue" type="radio" id="1" />
                             <label for="1">08:30 - 11:45</label>
                         </p>
                         <p>
-                            <input name="group1" class="blue" type="radio" id="2" />
+                            <input name="time" class="blue" type="radio" id="2" />
                             <label for="2">12:30 - 16:30</label>
                         </p>
                         <p>
-                            <input name="group1" class="blue" type="radio" id="3" />
+                            <input name="time" class="blue" type="radio" id="3" />
                             <label for="3">08:30 - 16:30</label>
                         </p>
                     </div>
                     <div class="row">
                         <button class="btn waves-effect blue waves-light right" type="submit">Reserveren</button>
+                        <input type="hidden" name="token" value="<?php echo Token::generate();?>">
                     </div>
                 </div>
             </form>
